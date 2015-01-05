58a16e25e3fc59fea0a2560d5e0b11712d5157a5
Stephanie Nemeth <traumverloren@gmail.com>
2015-01-05 21:41:02 +0100

    updated tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,11 +5,11 @@ class Article < ActiveRecord::Base
   has_many :tags, through: :taggings
 
   def tag_list
-    tags.join(', ')
+    tags.join(", ")
   end
 
-  def tag_list=(tags_string)
-    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+  def tag_list=(incoming_tag_string)
+    tag_names = incoming_tag_string.split(",").collect{|s| s.strip.downcase}.uniq
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
   end
